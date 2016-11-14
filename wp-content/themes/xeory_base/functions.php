ee5884e0026553b5eefae43f4987807784640dec
Takenoko <michaeelbobu@gmail.com>
2016-11-15 01:02:24 +0900

    アクションフック追加

--- wp-content/themes/xeory_base/functions.php
+++ wp-content/themes/xeory_base/functions.php
@@ -19,3 +19,16 @@ require_once('lib/functions/social_btn.php');
 require_once('lib/functions/show_avatar.php');
 require_once('lib/functions/shortcode.php');
 require_once('lib/functions/rss.php');
+
+
+//======================================================================
+// Custom
+//======================================================================
+function load_stylesheet() {
+  wp_enqueue_style('custom_style', get_bloginfo('template_directory').'/dist/css/app.css', array(), null, 'all');
+}
+function load_scripts() {
+  wp_enqueue_script( 'custom_scripts', get_bloginfo( 'template_directory') . '/dist/js/main.js', array(), false, 'true' );
+}
+add_action('wp_enqueue_scripts', 'load_stylesheet');
+add_action('wp_enqueue_scripts', 'load_scripts');
